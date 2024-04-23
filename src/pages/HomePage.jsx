af088b385f6a32f9849e52ffab58e9034ca33f15
m4scarade <charly.oudin1@gmail.com>
2024-04-23 16:55:33 +0200

    ajout react router dom

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+function HomePage() {
+  return (
+    <>
+    <div>HomePage</div>
+
+    <Link to='/compteutilisateur'>KOUKOU</Link>
+
+    </>
+
+  )
+}
+
+export default HomePage
---
+++ src/pages/compteUtilisateur.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function compteUtilisateur() {
+  return (
+    <>
+        <h1>bkjzefhjkl</h1>
+        <Link to='/compteutilisateur'>KOUKOU</Link>
+    </>
+  )
+}
+
+export default compteUtilisateur
