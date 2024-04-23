d5bf42027ab4b43ad761c4636e46008006968d04
Julian Giraud <julian@MacBook-Air-de-Julian.local>
2024-04-23 17:05:05 +0200

    correction du router dom pour l'accés à la page utilisateur

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
-import compteUtilisateur from './pages/compteUtilisateur'
+import CompteUtilisateur from './pages/compteUtilisateur'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
     <BrowserRouter>
       <Routes>
           <Route path='/' element={<HomePage />}/>
-          <Route path='/compteutilisateur' element={<compteUtilisateur />}/>
+          <Route path='/compteUtilisateur' element={<CompteUtilisateur />}/>
       </Routes>
     </BrowserRouter>
     </>
--- src/pages/compteUtilisateur.jsx
+++ src/pages/compteUtilisateur.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 function compteUtilisateur() {
   return (
     <>
         <h1>bkjzefhjkl</h1>
-        <Link to='/compteutilisateur'>KOUKOU</Link>
+        <Link to='/compteUtilisateur'>KOUKOU</Link>
     </>
   )
 }
