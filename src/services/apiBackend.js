6b7ffd85d0c10f8a2c64b9b744a087a9031c076d
m4scarade <charly.oudin1@gmail.com>
2024-04-23 18:33:32 +0200

    login et register

--- src/services/apiBackend.js
+++ src/services/apiBackend.js
@@ -1,12 +1,30 @@
 
-  export const enregistrement = async (register, username, password) => {
+  export const enregistrement = async (username2, password2) => {
     const resp = await fetch("http://fauques.freeboxos.fr:3000/register", {
       method: "POST",
       headers: {
-            "username": username,
-            "password": password    
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(register),
+      body: JSON.stringify({username: username2,
+      password: password2}),
     });
+    if (resp.status === 409){
+        console.log("Registration failed with 409, trying to login...");
+        let loginResp = await login(username2, password2);
+        return loginResp;
+      }
+    return await login(username2, password2);
+  };
+
+  export const login = async (username2, password2) => {
+    const resp = await fetch("http://fauques.freeboxos.fr:3000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({username: username2,
+      password: password2}),
+    });
+
     return resp.json();
   };
