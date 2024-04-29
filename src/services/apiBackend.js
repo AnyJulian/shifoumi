690196c3dc8f493f802e6e6ec3561a0b8b6d30bd
Julian Giraud <julian@MacBook-Air-de-Julian.local>
2024-04-29 13:40:48 +0200

    avancé mais ça marche pas

--- src/services/apiBackend.js
+++ src/services/apiBackend.js
@@ -1,41 +1,32 @@
 
 const apiCall = async (url, method, data) => {
-  try {
-    const resp = await fetch(url, {
-      method: method,
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-
-    if (resp.status === 409) {
-      return 409
-      // console.log("Échec de l'inscription avec le code 409, tentative de connexion...");
-      // const loginResp = await enregistrement("http://fauques.freeboxos.fr:3000/login", "POST", data);
-      // return loginResp;
-    }
-    return resp.json();
-
-  } catch (error) {
-    console.error("Erreur lors de l'appel API :", error);
-    throw error; // Gérer l'erreur en dehors de cette fonction
-  }
+  const resp = await fetch(url, {
+    method: method,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+  .then(response=> response.body.json())
+  .then(myJson=> console.log(myJson))
+return myJson
 };
 
 export const enregistrement = async (url, method, data) => {
-  const resp = apiCall (url, method, data);
+  console.log("Enregistrement en cours ...");
 
-  if (resp == 409) {
-    console.log("Échec de l'inscription avec le code 409, tentative de connexion...");
-    const loginResp = await enregistrement("http://fauques.freeboxos.fr:3000/login", "POST", data);
-    console.log(loginResp)
+  const resp = await apiCall (url, method, data);
+
+  console.log("retour api avant vérification : ", resp)
+  console.log("resp.error : ", resp.error)
+
+
+  if (resp.error === "User already exists") {
+    console.log("Échec de l'inscription, le compte existe déjà, tentative de connexion...");
+    const loginResp = await apiCall("http://fauques.freeboxos.fr:3000/login", method, data);
+    console.log("réponse api (login): ", loginResp)
   }
   else {
-    console.log("Enregistrement en cours");
-    const subResp = await enregistrement("http://fauques.freeboxos.fr:3000/login", "POST", data);
-    console.log(subResp)
-    const loginResp = await enregistrement("http://fauques.freeboxos.fr:3000/login", "POST", data);
-    console.log(subResp)
+    console.log("réponse api (register) : ", resp)
   }
 };
