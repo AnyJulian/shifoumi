84c5458466af681a48803096464c7cfb0131031e
AnyJulian <julian.giraud1369@gmail.com>
2024-05-05 15:31:49 +0200

    retour version beta de framer motion3D pour faire marcher le bouton play en 3D

--- src/pages/newHome.jsx
+++ src/pages/newHome.jsx
@@ -1,45 +1,83 @@
-import React from 'react'
 import ProgressButton from "../assets/buttonLocked";
-import { motion } from "framer-motion";
-
-const draw = {
-    hidden: { pathLength: 0, opacity: 0 },
-    visible: (i) => {
-      const delay = 1 + i * 0.5;
-      return {
-        pathLength: 1,
-        opacity: 1,
-        transition: {
-          pathLength: { delay, type: "spring", duration: 1.5, bounce: 0 },
-          opacity: { delay, duration: 0.01 }
-        }
-      };
-    }
-  };
+import { Suspense, useState } from "react";
+import { motion, MotionConfig, useMotionValue } from "framer-motion";
+import { Shapes } from "../config/shapes";
+import { transition } from "../config/settings";
+import useMeasure from "react-use-measure";
 
 function newHome() {
+  const [ref, bounds] = useMeasure({ scroll: false });
+  const [isHover, setIsHover] = useState(false);
+  const [isPress, setIsPress] = useState(false);
+  const mouseX = useMotionValue(0);
+  const mouseY = useMotionValue(0);
+
+  const resetMousePosition = () => {
+    mouseX.set(0);
+    mouseY.set(0);
+  };
+
   return (
     <>
-        <h1>newHome</h1>
-        <ProgressButton/>
-        <motion.svg
-        width="600"
-        height="600"
-        viewBox="0 0 600 600"
-        initial="hidden"
-        animate="visible"
+    <h1>newHome</h1>
+    <ProgressButton/>
+    <MotionConfig transition={transition}>
+      <motion.button
+        ref={ref}
+        initial={false}
+        animate={isHover ? "hover" : "rest"}
+        whileTap="press"
+        variants={{
+          rest: { scale: 1 },
+          hover: { scale: 1.5 },
+          press: { scale: 1.4 }
+        }}
+        onHoverStart={() => {
+          resetMousePosition();
+          setIsHover(true);
+        }}
+        onHoverEnd={() => {
+          resetMousePosition();
+          setIsHover(false);
+        }}
+        onTapStart={() => setIsPress(true)}
+        onTap={() => setIsPress(false)}
+        onTapCancel={() => setIsPress(false)}
+        onPointerMove={(e) => {
+          mouseX.set(e.clientX - bounds.x - bounds.width / 2);
+          mouseY.set(e.clientY - bounds.y - bounds.height / 2);
+        }}
+      >
+        <motion.div
+          className="shapes"
+          variants={{
+            rest: { opacity: 0 },
+            hover: { opacity: 1 }
+          }}
         >
-            <motion.circle
-                cx="100"
-                cy="100"
-                r="80"
-                stroke="#ff0055"
-                variants={draw}
-                custom={1}
-            />
-        </motion.svg>
-    </>
-  )
+          <div className="pink blush" />
+          <div className="blue blush" />
+          <div className="container">
+            <Suspense fallback={null}>
+              <Shapes
+                isHover={isHover}
+                isPress={isPress}
+                mouseX={mouseX}
+                mouseY={mouseY}
+              />
+            </Suspense>
+          </div>
+        </motion.div>
+        <motion.div
+          variants={{ hover: { scale: 0.85 }, press: { scale: 1.1 } }}
+          className="label"
+        >
+          play
+        </motion.div>
+      </motion.button>
+    </MotionConfig>
+  </>
+  );
 }
 
 export default newHome
